25e491b84b969bf9ec91cbe4afed3ffd7773f6c6
Nata <129781113+AkbarAgpranata@users.noreply.github.com>
2024-06-27 13:19:47 +0700

    Update About.js

--- src/About.js
+++ src/About.js
@@ -7,7 +7,7 @@ function About() {
       <h2>Sejarah Kampung Melayu</h2>
       <img src={gambar1} alt="SEJARAH" className="sejarah"/>
       <p>
-      Foto di atas adalah foto pendiri PSHT Ranting Kampung Melayu dan siswa pertama di Ranting Kampung Melayu. Cikal bakal berdirinya Persaudaraan Setia Hati Terate di Kampung Melayu pada tahun 2011 dimana saat itu latihan pertama diadakan di Stockpile batu bara Kelurahan Teluk Sepang, Kecamatan Kampung Melayu. Yang didirikan oleh mas Al Shokib, yang berasal dari Bojonegoro dan telah melahirkan beberapa pendekar yaitu, mas Fikri,Zeleski,Meriyanto, Budi,Widianto dan hingga sekarang telah berkembang menjadi beberapa rayon serta melahirkan pendekar-pendekar dan srikandi PSHT yang lainnya.
+      Foto di atas adalah foto pendiri PSHT Ranting Kampung Melayu dan siswa pertama di Ranting Kampung Melayu. Cikal bakal berdirinya Persaudaraan Setia Hati Terate di Kampung Melayu pada bulan November tahun 2010 dimana saat itu latihan pertama diadakan di Stockpile batu bara Kelurahan Teluk Sepang, Kecamatan Kampung Melayu. Yang didirikan oleh mas Al Shokib, yang berasal dari Bojonegoro dan telah melahirkan beberapa pendekar yaitu, mas Fikri,Zeleski,Meriyanto, Budi,Widianto dan hingga sekarang telah berkembang menjadi beberapa rayon serta melahirkan pendekar-pendekar dan srikandi PSHT yang lainnya.
     </p>
     </div>
   );
